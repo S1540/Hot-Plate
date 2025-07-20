7b483545c7b3e554a1e08b398a7564f0e8eb2061
Shubham Singh <shubhamkumar2452004@gmail.com>
2025-07-20 23:38:18 +0530

    solve api error

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -7,35 +7,35 @@ function FoodList() {
   };
   const [food, setFoods] = useState([]);
   useEffect(() => {
-    fetch(
-      "https://api.spoonacular.com/recipes/random?number=20&apiKey=d55e94f994f74d50a14a4b3e890dfb07"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-
-        setFoods(data.recipes);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    const fetchRandomFoods = async () => {
+      const allFoods = [];
+      for (let i = 0; i < 30; i++) {
+        const res = await fetch(
+          "https://www.themealdb.com/api/json/v1/1/random.php"
+        );
+        const data = await res.json();
+        allFoods.push(data.meals[0]);
+      }
+      setFoods(allFoods);
+    };
+    fetchRandomFoods();
   }, []);
 
   return (
     <>
-      <main className="max-w-full h-auto bg-green-100 px-4 py-2 flex justify-around flex-wrap ">
+      <main className="max-w-full h-auto bg-gradient-to-b from-amber-600 bg-fixed to-orange-200 px-4 py-2 flex justify-around gap-5 flex-wrap ">
         {food.map((item) => {
           return (
-            <div className=" max-w-72 h-auto  pb-3 rounded-md overflow-hidden bg-white hover:shadow-md shadow-orange-400 transition-all duration-300 ease-in-out hover:-translate-y-1">
-              <img src={item.image} alt="" className="rounded-md" />
+            <div className=" max-w-72 h-auto  pb-3 rounded-md overflow-hidden bg-orange-100 hover:shadow-md shadow-orange-400 transition-all duration-300 ease-in-out hover:-translate-y-1">
+              <img src={item.strMealThumb} alt="" className="rounded-md" />
               <div className="w-full flex justify-between pt-2 px-1">
                 {" "}
-                <h1>{item.title}</h1>
+                <h1>{item.strMeal}</h1>
                 <p></p>
               </div>
               <div className="w-full flex flex-col gap-2 px-1 py-1 ">
                 <p className="text-gray-600 text-xs">
-                  {removeHTMLTags(item.summary)}
+                  {removeHTMLTags(item.strCategory)}
                 </p>
                 <p className="text-green-500 text-xs font-bold">
                   {item.readyInMinutes}
